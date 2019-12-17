bcba9da7adbc334b6393822c82c2df38b1684c04
Betty-wu <1083973605@qq.com>
2019-12-17 19:21:19 +0800

    201912

---
+++ 第四周/12-16/hw/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('www'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: './pic'}).array('image'));
+
+
+app.post('/upload',(req,res)=>{
+    //当前图片的名字 req.files[0].originalname
+
+    // console.log(req.files[0]);
+    //图片上传到的文件夹路径:绝对路径+图片文件夹名称(要与设置的名称一样)+图片路径
+    let writePath = __dirname + "/img/" + req.files[0].originalname;//写的路径
+    //访问的路径
+    fs.readFile(req.files[0].path,(error,data)=>{
+        if(error){
+            console.log('404');
+        }else{
+            let o = fs.writeFileSync(writePath,data);
+            res.json({code:0});
+        }
+    });
+    // console.log();
+})
+
+
+
+app.listen(8080, ()=>{
+    console.log('8080 start');
+});

1fe1b03ab9646726dff26c5ad6e38a75919163d6
Betty-wu <1083973605@qq.com>
2019-12-30 09:25:10 +0800

    201912

--- 第六周/12-27/deploy/webpack.config.js
+++ 第六周/12-27/deploy/webpack.config.js
@@ -1,32 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+let path = require("path");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+let webpack = require("webpack");
 let obj = {
-    mode: "development",
-    entry: {
-        index: "./src/index.js"
-    },
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.less$/,
-                use: ["style-loader", "css-loader", "less-loader"] //这三个顺序一定要对,不然会报错
-            }
-        ]
-    },
-    plugins: [new htmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "index.html"
-    })]
-}
+        mode: "development",
+        entry: {
+            index: "./src/index.js"
+        },
 
-
-
-
-
-
-
-module.exports = obj;
+        module: {
+            rules: [{
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"]
+                },
+                {
+                    test: /\.less$/,
+                    use: ["style-loader", "css-loader", "less-loader"] //这三个顺序一定要对,不然会报错
+                }
+            ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "./src"),
+            historyApiFallback: true,
+            inline: true,
+            hot: true,
+            open: true,
+            port: 80
+        },
+        plugins: [new htmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })]}
+        module.exports = obj
